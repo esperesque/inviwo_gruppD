5f58619f1a3ac91535ec82949e348cc46f768ed9
carlsward <smannen00@gmail.com>
2025-04-23 13:43:09 +0200

    Add “Presentation View” dock-widget and View-menu entry
    Re-use existing PresentationViewPanel inside a new lazy-created InviwoDockWidget
    
    Added menu item under View → Presentation View; remembers dock state & icon
    
    Updated AnimationQtModule header/impl.: forward-declare InviwoDockWidget, keep a unique_ptr to the new widget, include panel & dock headers, register via addWidgetLazy()

--- modules/animationqt/include/modules/animationqt/animationqtmodule.h
+++ modules/animationqt/include/modules/animationqt/animationqtmodule.h
@@ -1,46 +1,27 @@
 /*********************************************************************************
- *
  * Inviwo - Interactive Visualization Workshop
- *
- * Copyright (c) 2016-2025 Inviwo Foundation
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
  *********************************************************************************/
 #pragma once
 
-#include <modules/animationqt/animationqtmoduledefine.h>  // for IVW_MODULE_ANIMATIONQT_API
+#include <modules/animationqt/animationqtmoduledefine.h>
 
-#include <inviwo/core/common/inviwomodule.h>                      // for InviwoModule
-#include <modules/animationqt/animationqtsupplier.h>              // for AnimationQtSupplier
-#include <modules/animationqt/factories/sequenceeditorfactory.h>  // for SequenceEditorFactory
-#include <modules/animationqt/factories/trackwidgetqtfactory.h>   // for TrackWidgetQtFactory
+#include <inviwo/core/common/inviwomodule.h>
+#include <modules/animationqt/animationqtsupplier.h>
+#include <modules/animationqt/factories/sequenceeditorfactory.h>
+#include <modules/animationqt/factories/trackwidgetqtfactory.h>
 
-#include <memory>  // for unique_ptr
+#include <memory>
 
-class QMenu;
+class QMenu;  // <- OK att ligga globalt, används utan namespace
 
+/* ------------------------------------------------------------------ */
+/*  ALLT UNDER LIGGER NU INUTI NAMESPACE INVIWO                        */
+/* ------------------------------------------------------------------ */
 namespace inviwo {
+
 class InviwoApplication;
+/* ►► Lägg forward-deklarationen HÄR istället för globalt ◄◄ */
+class InviwoDockWidget;  // <- nu samma typ som i .cpp
 
 namespace animation {
 class AnimationEditorDockWidgetQt;
@@ -49,6 +30,7 @@ class DemoNavigatorDockWidgetQt;
 
 class IVW_MODULE_ANIMATIONQT_API AnimationQtModule : public InviwoModule,
                                                      public animation::AnimationQtSupplier {
+
 public:
     AnimationQtModule(InviwoApplication* app);
     virtual ~AnimationQtModule();
@@ -60,14 +42,16 @@ public:
     const animation::SequenceEditorFactory& getSequenceEditorFactory() const;
 
 private:
+    /* factories */
     animation::TrackWidgetQtFactory trackWidgetQtFactory_;
     animation::SequenceEditorFactory sequenceEditorFactory_;
 
-    // Keep references to added widgets so that they can be removed in destructor
+    /* widgets vi behåller pekare till */
     std::unique_ptr<animation::AnimationEditorDockWidgetQt> editor_;
     std::unique_ptr<animation::DemoNavigatorDockWidgetQt> navigator_;
+    std::unique_ptr<InviwoDockWidget> presentationView_;  // ← samma typ
 
-    std::unique_ptr<QMenu> menu_;  // Show/hide animation editor
+    std::unique_ptr<QMenu> menu_;
 };
 
 }  // namespace inviwo
--- modules/animationqt/src/animationqtmodule.cpp
+++ modules/animationqt/src/animationqtmodule.cpp
@@ -5,89 +5,72 @@
  * Copyright (c) 2016-2025 Inviwo Foundation
  * All rights reserved.
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- * list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- * this list of conditions and the following disclaimer in the documentation
- * and/or other materials provided with the distribution.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
  *********************************************************************************/
 
 #include <modules/animationqt/animationqtmodule.h>
 
-#include <inviwo/core/common/inviwomodule.h>                            // for InviwoModule
-#include <inviwo/core/datastructures/camera/camera.h>                   // for mat4
-#include <inviwo/core/properties/boolproperty.h>                        // for BoolProperty
-#include <inviwo/core/properties/cameraproperty.h>                      // for CameraProperty
-#include <inviwo/core/properties/fileproperty.h>                        // for FileProperty
-#include <inviwo/core/properties/minmaxproperty.h>                      // for MinMaxProperty
-#include <inviwo/core/properties/optionproperty.h>                      // for OptionProperty
-#include <inviwo/core/properties/ordinalproperty.h>                     // for OrdinalProperty
-#include <inviwo/core/properties/ordinalrefproperty.h>                  // for OrdinalRefProperty
-#include <inviwo/core/properties/stringproperty.h>                      // for StringProperty
-#include <inviwo/core/util/foreacharg.h>                                // for for_each_type
-#include <inviwo/core/util/glmmat.h>                                    // for dmat2, dmat3, dmat4
-#include <inviwo/core/util/glmvec.h>                                    // for dvec2, dvec3, dvec4
-#include <inviwo/core/util/moduleutils.h>                               // for getModuleByType
-#include <inviwo/core/util/raiiutils.h>                                 // for OnScopeExit
-#include <inviwo/core/util/staticstring.h>                              // for operator+
-#include <modules/animation/animationmodule.h>                          // for AnimationModule
-#include <modules/animation/datastructures/buttontrack.h>               // for ButtonTrack
-#include <modules/animation/datastructures/callbacktrack.h>             // for CallbackTrack
-#include <modules/animation/datastructures/invalidationtrack.h>         // for InvalidationTrack
-#include <modules/animation/datastructures/camerakeyframe.h>            // for CameraKeyframe
-#include <modules/animation/datastructures/cameratrack.h>               // IWYU pragma: keep
-#include <modules/animation/datastructures/controltrack.h>              // for ControlTrack
-#include <modules/animation/datastructures/propertytrack.h>             // for PropertyTrack
-#include <modules/animation/datastructures/valuekeyframe.h>             // for ValueKeyframe
-#include <modules/animation/datastructures/valuekeyframesequence.h>     // for KeyframeSequenceT...
-#include <modules/animation/interpolation/interpolation.h>              // for InterpolationTyped
-#include <modules/animationqt/animationeditordockwidgetqt.h>            // for AnimationEditorDo...
-#include <modules/animationqt/animationqtsupplier.h>                    // for AnimationQtSupplier
-#include <modules/animationqt/demo/demonavigatordockwidgetqt.h>         // for DemoNavigatorDock...
-#include <modules/animationqt/factories/sequenceeditorfactoryobject.h>  // for animation
-#include <modules/animationqt/sequenceeditor/controlsequenceeditor.h>   // for ControlSequenceEd...
-#include <modules/animationqt/sequenceeditor/invalidationsequenceeditor.h>  // for ControlSequenceEd...
-#include <modules/animationqt/sequenceeditor/propertysequenceeditor.h>  // for PropertySequenceE...
-#include <modules/animationqt/widgets/controltrackwidgetqt.h>           // for ControlTrackWidgetQt
-#include <modules/animationqt/widgets/propertytrackwidgetqt.h>          // for PropertyTrackWidg...
-#include <modules/qtwidgets/inviwoqtutils.h>                            // for getApplicationMai...
+#include <inviwo/core/common/inviwomodule.h>
+#include <inviwo/core/datastructures/camera/camera.h>
+#include <inviwo/core/properties/boolproperty.h>
+#include <inviwo/core/properties/cameraproperty.h>
+#include <inviwo/core/properties/fileproperty.h>
+#include <inviwo/core/properties/minmaxproperty.h>
+#include <inviwo/core/properties/optionproperty.h>
+#include <inviwo/core/properties/ordinalproperty.h>
+#include <inviwo/core/properties/ordinalrefproperty.h>
+#include <inviwo/core/properties/stringproperty.h>
+#include <inviwo/core/util/foreacharg.h>
+#include <inviwo/core/util/glmmat.h>
+#include <inviwo/core/util/glmvec.h>
+#include <inviwo/core/util/moduleutils.h>
+#include <inviwo/core/util/raiiutils.h>
+#include <inviwo/core/util/staticstring.h>
 
-#include <algorithm>   // for find_if
-#include <cstddef>     // for size_t
-#include <functional>  // for __base
-#include <string>      // for string
-#include <tuple>       // for tuple
-#include <vector>      // for vector
+#include <modules/animation/animationmodule.h>
+#include <modules/animation/datastructures/buttontrack.h>
+#include <modules/animation/datastructures/callbacktrack.h>
+#include <modules/animation/datastructures/invalidationtrack.h>
+#include <modules/animation/datastructures/camerakeyframe.h>
+#include <modules/animation/datastructures/cameratrack.h>
+#include <modules/animation/datastructures/controltrack.h>
+#include <modules/animation/datastructures/propertytrack.h>
+#include <modules/animation/datastructures/valuekeyframe.h>
+#include <modules/animation/datastructures/valuekeyframesequence.h>
+#include <modules/animation/interpolation/interpolation.h>
 
-#include <QAction>         // for QAction
-#include <QList>           // for QList, QList<>::i...
-#include <QMainWindow>     // for QMainWindow
-#include <QMenu>           // for QMenu
-#include <QMenuBar>        // for QMenuBar
-#include <QObject>         // for QObject
-#include <QString>         // for QString
-#include <Qt>              // for DockWidgetArea
-#include <glm/mat2x2.hpp>  // for operator+
-#include <glm/mat3x3.hpp>  // for operator+
-#include <glm/mat4x4.hpp>  // for operator+
-#include <glm/vec2.hpp>    // for operator+
-#include <glm/vec3.hpp>    // for operator+
-#include <glm/vec4.hpp>    // for operator+
+#include <modules/animationqt/animationeditordockwidgetqt.h>
+#include <modules/animationqt/animationqtsupplier.h>
+#include <modules/animationqt/demo/demonavigatordockwidgetqt.h>
+#include <modules/animationqt/factories/sequenceeditorfactoryobject.h>
+#include <modules/animationqt/sequenceeditor/controlsequenceeditor.h>
+#include <modules/animationqt/sequenceeditor/invalidationsequenceeditor.h>
+#include <modules/animationqt/sequenceeditor/propertysequenceeditor.h>
+#include <modules/animationqt/widgets/controltrackwidgetqt.h>
+#include <modules/animationqt/widgets/propertytrackwidgetqt.h>
+
+#include <modules/animationqt/presentationviewpanel.h>  // Presentation-fönstret
+#include <modules/qtwidgets/inviwodockwidget.h>         // InviwoDockWidget
+#include <modules/qtwidgets/inviwoqtutils.h>
+
+#include <algorithm>
+#include <tuple>
+#include <vector>
+
+#include <QAction>
+#include <QIcon>
+#include <QMainWindow>
+#include <QMenu>
+#include <QMenuBar>
+#include <QObject>
+#include <QString>
+#include <Qt>
+
+#include <glm/mat2x2.hpp>
+#include <glm/mat3x3.hpp>
+#include <glm/mat4x4.hpp>
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
 
 namespace inviwo {
 namespace animation {
@@ -98,8 +81,6 @@ class TrackWidgetQtFactory;
 
 #ifndef INVIWO_ALL_DYN_LINK
 struct InitQtAnimationResources {
-    // Needed for loading of resources when building statically
-    // see https://wiki.qt.io/QtResources#Q_INIT_RESOURCE
     InitQtAnimationResources() { Q_INIT_RESOURCE(animation); }
     ~InitQtAnimationResources() { Q_CLEANUP_RESOURCE(animation); }
 } initQtAnimationResources;
@@ -107,7 +88,9 @@ struct InitQtAnimationResources {
 
 namespace inviwo {
 
-class InviwoApplication;
+/* ------------------------------------------------------------------------- */
+/*                               Hjälpfunktioner                              */
+/* ------------------------------------------------------------------------- */
 
 namespace {
 
@@ -115,12 +98,10 @@ template <typename PropertyType, typename Keyframe,
           typename KeyframeSequence = animation::KeyframeSequenceTyped<Keyframe>>
 void registerPropertyTrackHelper(animation::AnimationQtSupplier& as) {
     using namespace animation;
-
     using TrackType = PropertyTrack<PropertyType, Keyframe, KeyframeSequence>;
 
     as.registerTrackToWidgetMap(TrackType::classIdentifier(),
                                 PropertyTrackWidgetQt::classIdentifier());
-
     as.registerTrackToSequenceEditorMap(TrackType::classIdentifier(),
                                         PropertySequenceEditor::classIdentifier());
 }
@@ -136,6 +117,10 @@ struct Reghelper {
 
 }  // namespace
 
+/* ------------------------------------------------------------------------- */
+/*                               Konstruktor                                 */
+/* ------------------------------------------------------------------------- */
+
 AnimationQtModule::AnimationQtModule(InviwoApplication* app)
     : InviwoModule(app, "AnimationQt")
     , animation::AnimationQtSupplier(*this)
@@ -144,36 +129,33 @@ AnimationQtModule::AnimationQtModule(InviwoApplication* app)
 
     using namespace animation;
 
+    /* -------------------- skapa/utöka View-menyn -------------------------- */
     if (auto win = utilqt::getApplicationMainWindow()) {
         QString animationMenuName("Animation");
         QMenu* menu = nullptr;
-        // Find view menu
+
         auto menus = win->menuBar()->findChildren<QMenu*>();
         auto viewMenuIt = std::find_if(menus.begin(), menus.end(), [](auto& m) {
             return m->title().compare(QObject::tr("&View"), Qt::CaseInsensitive) == 0;
         });
 
         if (viewMenuIt != menus.end()) {
-            // Add to view menu if found
             menu = (*viewMenuIt);
         } else {
-            // Add new menu if not found
-            // To be removed when module is destroyed
             menu_ = std::make_unique<QMenu>(animationMenuName);
             win->menuBar()->addMenu(menu_.get());
             menu = menu_.get();
-            // Release pointer if destroyed by Qt before module is destroyed
             QObject::connect(menu_.get(), &QObject::destroyed, [&](QObject*) { menu_.release(); });
         }
 
+        /* ------ hjälpare som skapar widgeten första gången man klickar ----- */
         const auto addWidgetLazy = [&](QString name, Qt::DockWidgetArea area, auto factory) {
             auto* action = menu->addAction(name);
             action->setCheckable(true);
             QObject::connect(action, &QAction::triggered, [action, win, area, menu, factory]() {
-                // The first time the callback is called we will create the widget. And then replace
-                // this action with the toggleViewAction from the new widget.
-                util::OnScopeExit onExit([action]() { delete action; });
-                auto widget = factory();
+                util::OnScopeExit cleanup([action]() { delete action; });
+
+                auto widget = factory();  // skapas nu
                 win->addDockWidget(area, widget);
                 widget->hide();
                 widget->loadState();
@@ -184,30 +166,51 @@ AnimationQtModule::AnimationQtModule(InviwoApplication* app)
             });
         };
 
+        /* ---------------------- Animation Editor -------------------------- */
         addWidgetLazy("Animation Editor", Qt::BottomDockWidgetArea, [this, win]() {
             auto* animationModule = util::getModuleByType<AnimationModule>();
             editor_ = std::make_unique<AnimationEditorDockWidgetQt>(
                 animationModule->getWorkspaceAnimations(), animationModule->getAnimationManager(),
                 "Animation Editor", getTrackWidgetQtFactory(), getSequenceEditorFactory(), win);
-            // Release pointer if destroyed by Qt before module is destroyed
+
             QObject::connect(editor_.get(), &QObject::destroyed,
                              [this](QObject*) { editor_.release(); });
             return editor_.get();
         });
 
+        /* ---------------------- Demo Navigator ---------------------------- */
         addWidgetLazy("Demo Navigator", Qt::RightDockWidgetArea, [this, win]() {
             auto* animationModule = util::getModuleByType<AnimationModule>();
             auto& demoController = animationModule->getDemoController();
             navigator_ =
                 std::make_unique<DemoNavigatorDockWidgetQt>(demoController, "Demo Navigator", win);
-            // Release pointer if destroyed by Qt before module is destroyed
+
             QObject::connect(navigator_.get(), &QObject::destroyed,
                              [this](QObject*) { navigator_.release(); });
             return navigator_.get();
         });
+
+        /* ---------------------- Presentation View (NY) ------------------- */
+        addWidgetLazy("Presentation View", Qt::BottomDockWidgetArea, [this, win]() {
+            /* Skapa själva dock-widgeten */
+            auto* dock = new InviwoDockWidget(utilqt::toQString(std::string("Presentation View")),
+                                              win, "PresentationViewWidget");
+
+            dock->setFloating(true);
+            dock->setSticky(true);
+            dock->setWindowIcon(QIcon(":/animation/icons/monitor.svg"));
+            dock->setWidget(new PresentationViewPanel());
+
+            presentationView_.reset(dock);  // spara pekaren
+
+            QObject::connect(dock, &QObject::destroyed,
+                             [this](QObject*) { presentationView_.release(); });
+            return dock;
+        });
     }
 
-    // register widgets
+    /* ------------------- registrera track-widgets m.m. -------------------- */
+
     registerTrackWidgetQt<PropertyTrackWidgetQt>();
     registerTrackWidgetQt<ControlTrackWidgetQt>();
 
@@ -215,10 +218,10 @@ AnimationQtModule::AnimationQtModule(InviwoApplication* app)
     registerSequenceEditor<ControlSequenceEditor>();
     registerSequenceEditor<InvalidationSequenceEditor>();
 
-    // Map Ordinal properties
     using Types = std::tuple<float, vec2, vec3, vec4, mat2, mat3, mat4, double, dvec2, dvec3, dvec4,
                              dmat2, dmat3, dmat4, int, ivec2, ivec3, ivec4, unsigned int, uvec2,
                              uvec3, uvec4, size_t, size2_t, size3_t, size4_t>;
+
     util::for_each_type<Types>{}(Reghelper<OrdinalProperty>{}, *this);
     util::for_each_type<Types>{}(Reghelper<OrdinalRefProperty>{}, *this);
 
@@ -237,33 +240,31 @@ AnimationQtModule::AnimationQtModule(InviwoApplication* app)
 
     registerTrackToWidgetMap(ButtonTrack::classIdentifier(),
                              PropertyTrackWidgetQt::classIdentifier());
-
     registerTrackToWidgetMap(CallbackTrack::classIdentifier(),
                              PropertyTrackWidgetQt::classIdentifier());
-
     registerTrackToWidgetMap(ControlTrack::classIdentifier(),
                              ControlTrackWidgetQt::classIdentifier());
-
     registerTrackToWidgetMap(InvalidationTrack::classIdentifier(),
                              ControlTrackWidgetQt::classIdentifier());
 
     registerTrackToSequenceEditorMap(ControlTrack::classIdentifier(),
                                      ControlSequenceEditor::classIdentifier());
-
     registerTrackToSequenceEditorMap(InvalidationTrack::classIdentifier(),
                                      InvalidationSequenceEditor::classIdentifier());
+}
 
-}  // namespace inviwo
+/* ------------------------------------------------------------------------- */
+/*                                  Destruktor                               */
+/* ------------------------------------------------------------------------- */
 
 AnimationQtModule::~AnimationQtModule() {
-    // Unregister everything from the factory since this module _owns_ the factory. This is
-    // neccessary even though the base class destructor, i.e. animation::AnimationQtSupplier, takes
-    // care of this. Otherwise the supplier will unregister the items _after_ the factory is
-    // destroyed.
-    //
-    // Other modules do not have to do this!
+    /* Se till att unregister sker innan fabrikerna förstörs */
     unRegisterAll();
 }
+
+/* ------------------------------------------------------------------------- */
+/*                               Access-funktioner                           */
+/* ------------------------------------------------------------------------- */
 
 animation::TrackWidgetQtFactory& AnimationQtModule::getTrackWidgetQtFactory() {
     return trackWidgetQtFactory_;
